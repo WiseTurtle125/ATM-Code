aed670ac97e119927ccef91e8100cd9086e3f832
James Tung <james.tung@student.tdsb.on.ca>
2023-12-14 13:03:32 -0500

    db enum + template created

--- Main.java
+++ Main.java
@@ -1,4 +1,15 @@
 public class Main {
+    enum db {  // Used for indexing each line of the database
+        FIRST = 0,
+        LAST = 1,
+        NUM = 2,
+        PIN = 3,
+        SAVINGS = 4,
+        CHECKING = 5
+    }
+
+    final String TEMPLATE = "%s,%s,%d,%d,%f,%f";  // Used for formatting database entries
+
     public static void main(String[] args) {
         // Log in
     }
